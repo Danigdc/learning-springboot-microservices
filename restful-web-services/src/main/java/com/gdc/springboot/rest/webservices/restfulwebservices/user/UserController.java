9c608a188cfffc37437e7755949ddebca050b3e4
Danigdc <danigdcprojects@gmail.com>
2018-11-22 22:40:50 +0100

    Some rest stuff

---
+++ restful-web-services/src/main/java/com/gdc/springboot/rest/webservices/restfulwebservices/user/UserController.java
@@ -0,0 +1,25 @@
+package com.gdc.springboot.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUserById(@PathVariable int id){
+        return userDaoService.findOne(id);
+    }
+}
